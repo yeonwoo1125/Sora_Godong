3e1dc0622e49eb3c617eb5198a3444b4f53668c5
apf_temp_admin <s2110@e-mirim.hs.kr>
2022-02-11 01:06:50 +0900

    7초로 변경

--- app/src/main/java/mirim/msg/sora_godong/Luck.java
+++ app/src/main/java/mirim/msg/sora_godong/Luck.java
@@ -31,7 +31,6 @@ public class Luck extends AppCompatActivity {
     LuckAnswer.myDBHelper myDBHelper;
     SQLiteDatabase sqlDB;
     Animation luck_anim;
-    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,11 +47,11 @@ public class Luck extends AppCompatActivity {
         String getTime = sdf.format(date);
 
         sqlDB = myDBHelper.getReadableDatabase();
+        sqlDB.execSQL("delete from luckTB where testDate = '2022-02-11'");
 
         Cursor cursor = sqlDB.rawQuery("SELECT * FROM luckTB where testDate = '" + getTime + "';", null);
 
         luck_anim= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.luck_anim);
-        image=findViewById(R.id.image);
 
         if(cursor.getCount() == 1){
             setContentView(R.layout.activity_luckanswer);
@@ -90,7 +89,7 @@ public class Luck extends AppCompatActivity {
             btn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    image.startAnimation(luck_anim);
+                    btn.startAnimation(luck_anim);
                     Intent intent = new Intent(getApplicationContext(), LuckAnswer.class);
                     startActivity(intent);
                     finish();
