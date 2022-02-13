aa8dbfb398fdf41d1a31feed80f86974fe1494a6
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2022-02-13 17:24:34 +0900

    ask main commit / background change

---
+++ app/src/main/java/mirim/msg/sora_godong/AskLove.java
@@ -0,0 +1,13 @@
+package mirim.msg.sora_godong;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AskLove extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ask_love);
+    }
+}
