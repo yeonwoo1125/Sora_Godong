d01e3421d271c7da78d63fbd9112e8805bcdce1a
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2022-02-13 22:22:28 +0900

    layout change

--- app/src/main/java/mirim/msg/sora_godong/AskLove.java
+++ app/src/main/java/mirim/msg/sora_godong/AskLove.java
@@ -1,13 +1,30 @@
 package mirim.msg.sora_godong;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AskLove extends AppCompatActivity {
+
+    Button answer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ask_love);
+
+        answer = findViewById(R.id.btn_answer);
+
+        answer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AskAnswer.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
