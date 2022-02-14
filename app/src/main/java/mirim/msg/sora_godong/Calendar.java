db3e1cf55de0ce4e6f0c481a0e2b03b437e7f4b7
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-02-14 13:31:47 +0900

    Yeonwoo (#1)
    * [UPDATE] 마진 변경 및 EditText 높이 수정
    
    * [UPDATE] Dialog 설정 삭제
    
    * [ADD] 선택된 날짜 가져오기 및 저장된 날짜 확인 하는 기능 구현

--- app/src/main/java/mirim/msg/sora_godong/Calendar.java
+++ app/src/main/java/mirim/msg/sora_godong/Calendar.java
@@ -1,7 +1,8 @@
 package mirim.msg.sora_godong;
 
-import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.nfc.Tag;
@@ -23,6 +24,9 @@ import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
+
+public class Calendar extends AppCompatActivity{
+
 import org.w3c.dom.Text;
 
 import java.io.FileNotFoundException;
@@ -38,6 +42,9 @@ public class Calendar extends AppCompatActivity{
     //캘린더 커스텀을 위해 깃허브 라이브러리를 사용
     MaterialCalendarView calendarView;
 
+    private DbHelper dbHelper;
+    private SQLiteDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,10 +53,12 @@ public class Calendar extends AppCompatActivity{
 
         calendarView = (MaterialCalendarView) findViewById(R.id.calendar);
 
-        public String getToday_date() {
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-            return simpleDateFormat.format((Date) calendarView.getSelectedDate().getDate());
-        }
+        //날짜 선택 시 다이어리 페이지 열림
+        calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+        @Override
+        public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+            Intent intent = new Intent(getApplicationContext(), Diary.class);
+
 
 
         //날짜가 선택된 경우 실행하는 리스너
@@ -60,20 +69,14 @@ public class Calendar extends AppCompatActivity{
         @Override
         public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
             Intent intent = new Intent(getApplicationContext(), Diary.class);
-            int year = widget.getSelectedDate().getYear();
-            int month = widget.getSelectedDate().getMonth();
-            int day = widget.getSelectedDate().getDay();
 
-            String Today_date = year + "-" + month+1 + "-" + day;
-            Toast.makeText(getApplicationContext(), Today_date, Toast.LENGTH_SHORT).show();
-            //선택한 날짜를 Diary 페이지로 넘김
-            intent.putExtra("date", calendarView.getSelectedDate().toString());
             startActivity(intent);
             }
         });
-
-        //유저의 질문을 받아 question에 적용하는 메서드
-
-        //보살의 답을 받아 answer에 적용하는 메서드
     }
+          
+        public String getToday_date() {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+            return simpleDateFormat.format((Date) calendarView.getSelectedDate().getDate()).toString();
+        }
 }
