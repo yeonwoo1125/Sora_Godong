352f8a74c9029307b88864e411c4716153f64e2e
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-02-14 13:38:26 +0900

    [UPDATE] 머지 해결

--- app/src/main/java/mirim/msg/sora_godong/Calendar.java
+++ app/src/main/java/mirim/msg/sora_godong/Calendar.java
@@ -3,20 +3,8 @@ package mirim.msg.sora_godong;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.nfc.Tag;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CalendarView;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 import java.util.Date;
 
-import androidx.annotation.ContentView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -24,17 +12,8 @@ import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
-
-public class Calendar extends AppCompatActivity{
-
-import org.w3c.dom.Text;
-
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
-import java.util.Locale;
+
 
 public class Calendar extends AppCompatActivity{
 
@@ -55,28 +34,16 @@ public class Calendar extends AppCompatActivity{
 
         //날짜 선택 시 다이어리 페이지 열림
         calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
-        @Override
-        public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
-            Intent intent = new Intent(getApplicationContext(), Diary.class);
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                Intent intent = new Intent(getApplicationContext(), Diary.class);
+                startActivity(intent);
 
-
-
-        //날짜가 선택된 경우 실행하는 리스너
-        //Diary 페이지를 보여줌
-        calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
-
-
-        @Override
-        public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
-            Intent intent = new Intent(getApplicationContext(), Diary.class);
-
-            startActivity(intent);
             }
         });
     }
-          
-        public String getToday_date() {
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
-            return simpleDateFormat.format((Date) calendarView.getSelectedDate().getDate()).toString();
-        }
+    public String getToday_date () {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+        return simpleDateFormat.format((Date) calendarView.getSelectedDate().getDate()).toString();
+    }
 }
