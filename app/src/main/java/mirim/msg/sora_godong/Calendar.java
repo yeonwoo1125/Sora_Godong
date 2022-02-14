43f21ad49d1215860bd262f154d3806f821c492e
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-02-14 13:28:06 +0900

    [ADD] 선택된 날짜 가져오기 및 저장된 날짜 확인 하는 기능 구현

--- app/src/main/java/mirim/msg/sora_godong/Calendar.java
+++ app/src/main/java/mirim/msg/sora_godong/Calendar.java
@@ -1,14 +1,8 @@
 package mirim.msg.sora_godong;
 
-import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CalendarView;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,39 +11,32 @@ import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
-public class Calendar extends AppCompatActivity {
+public class Calendar extends AppCompatActivity{
+
 
     //캘린더 커스텀을 위해 깃허브 라이브러리를 사용
     MaterialCalendarView calendarView;
+
+    private DbHelper dbHelper;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
+
         calendarView = (MaterialCalendarView) findViewById(R.id.calendar);
 
-        //날짜가 선택된 경우 실행하는 리스너
-        //Diary 페이지를 보여줌
+        //날짜 선택 시 다이어리 페이지 열림
         calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
-            @Override
-            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
-                Intent intent=new Intent(getApplicationContext(),Diary.class);
-
-                //선택한 날짜를 Diary 페이지로 넘김
-                intent.putExtra("date",calendarView.getSelectedDate().toString());
-                startActivity(intent);
+        @Override
+        public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+            Intent intent = new Intent(getApplicationContext(), Diary.class);
+            startActivity(intent);
             }
         });
-
-        //유저의 질문을 받아 question에 적용하는 메서드
-
-        //보살의 답을 받아 answer에 적용하는 메서드
 
     }
 }
--- app/src/main/java/mirim/msg/sora_godong/DbHelper.java
+++ app/src/main/java/mirim/msg/sora_godong/DbHelper.java
@@ -2,15 +2,22 @@ package mirim.msg.sora_godong;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 //db와 관련된 작업을 하는 클래스
 public class DbHelper extends SQLiteOpenHelper {
-    static final String DATABASE_NAME = "calendar_db";
+    private static final String DATABASE_NAME = "calendar_db";
+    private final String TABLE_NAME = "calendar";
 
     private Context context;
     private static final String TODAY_DATE = "today_date";
@@ -52,7 +59,26 @@ public class DbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
-        db.execSQL("DROP TABLE IF EXISTS calendar");
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
         onCreate(db);
+    }
+
+    //캘린더의 날짜를 받아옴(yyyyMMdd)
+    public String getToday_date(MaterialCalendarView cv) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+        return simpleDateFormat.format((Date) cv.getSelectedDate().getDate()).toString();
+    }
+
+    //db에 저장된 날짜 확인
+    public String selectDate(SQLiteDatabase db, String selectDate){
+        String sql = "SELECT today_date FROM "+TABLE_NAME+" WHERE today_date = "+selectDate;
+        Cursor cursor = db.rawQuery(sql, null);
+        String date="";
+        while(cursor.moveToNext()){
+            date = cursor.getString(0);
+        }
+        cursor.close();
+
+        return date;
     }
 }
